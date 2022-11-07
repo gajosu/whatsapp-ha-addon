cdd0e53816235b043e36ec2e71d7b1684a61696f
Gabriel Gonzalez <gajosu15@gmail.com>
2022-11-07 12:44:40 -0500

    fix chromium path

--- whatsapp/src/whatsapp.js
+++ whatsapp/src/whatsapp.js
@@ -2,8 +2,8 @@ const { Client, LocalAuth } = require('whatsapp-web.js');
 const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
-        //chromium path
-        executablePath: '/usr/bin/chromium',
+        //chromium path in alpine
+        executablePath: '/usr/bin/chromium-browser',
         headless: true,
         args: [
             '--no-sandbox',
